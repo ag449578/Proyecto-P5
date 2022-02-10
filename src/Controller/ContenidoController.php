ac69908792df673be8c79ac27c703cc57552bf12
alexandergd <ag449578@gmail.com>
2022-02-10 16:23:39 -0500

    Terminado el CRUD Contendio(Implementada la subida de archivo)
    Signed-off-by: alexandergd <ag449578@gmail.com>

--- src/Controller/ContenidoController.php
+++ src/Controller/ContenidoController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Contenido;
 use App\Form\ContenidoType;
 use App\Repository\ContenidoRepository;
 use App\Repository\ProfesorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Mime\FileinfoMimeTypeGuesser;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -34,13 +39,27 @@ class ContenidoController extends AbstractController
     /**
      * @Route("/new", name="contenido_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, string $contenidoDir): Response
     {
         $contenido = new Contenido();
         $form = $this->createForm(ContenidoType::class, $contenido);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //para guardar nombre y url del archivo
+            $archivo = $form->get('archivo')->getData();
+            if ($archivo) {
+                
+                $filename = pathinfo($archivo->getClientOriginalName(), PATHINFO_FILENAME).'.'.$archivo->guessExtension();
+                
+                try {
+                    $archivo->move($contenidoDir, $filename);
+                } catch (FileException $e) {
+
+                }
+                $contenido->setUrlArchivo($filename);
+            }
+
             $entityManager->persist($contenido);
             $entityManager->flush();
 
@@ -54,24 +73,28 @@ class ContenidoController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="contenido_show", methods={"GET"})
-     */
-    public function show(Contenido $contenido): Response
-    {
-        return $this->render('/profesor/contenido/show.html.twig', [
-            'contenido' => $contenido,
-        ]);
-    }
-
-    /**
      * @Route("/{id}/edit", name="contenido_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Contenido $contenido, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Contenido $contenido, EntityManagerInterface $entityManager, string $contenidoDir): Response
     {
         $form = $this->createForm(ContenidoType::class, $contenido);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //para guardar nombre y url del archivo
+            $archivo = $form->get('archivo')->getData();
+            if ($archivo) {
+
+                $filename = pathinfo($archivo->getClientOriginalName(), PATHINFO_FILENAME) . '.' . $archivo->guessExtension();
+
+                try {
+                    $archivo->move($contenidoDir, $filename);
+                } catch (FileException $e) {
+
+                }
+                $contenido->setUrlArchivo($filename);
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('contenido_index', [], Response::HTTP_SEE_OTHER);
@@ -94,6 +117,40 @@ class ContenidoController extends AbstractController
         }
 
         return $this->redirectToRoute('contenido_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    /**
+     * @Route("/{id}", name="contenido_show", methods={"GET"})
+     */
+    public function show(Contenido $contenido): Response
+    {
+        return $this->render('/profesor/contenido/show.html.twig', [
+            'contenido' => $contenido,
+        ]);
+    }
+
+    /**
+     * @Route("/descarga/{nombre}", name="descarga", methods={"GET"})
+     */
+    public function descarga(string $nombre, string $contenidoDir):Response
+    {
+        $ruta=$contenidoDir.$nombre;
+        $response=new BinaryFileResponse($ruta);
+        $mime=new FileinfoMimeTypeGuesser();
+        $name=$nombre;
+
+        if ($mime->isGuesserSupported()) {
+            $response->headers->set('Content-Type',$mime->guessMimeType($contenidoDir.'112cf0811f35.pdf'));
+        }else{
+            $response->headers->set('Content-Type', 'text/plain');
+        }
+
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $name
+        );
+
+        return $response;
     }
 }
 
