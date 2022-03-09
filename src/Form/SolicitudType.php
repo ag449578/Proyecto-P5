261366441836cfe429f98d4c4040cbd555a62e71
Will781227 <willfreaum@gmail.com>
2022-03-09 14:55:54 -0500

    after pull

---
+++ src/Form/SolicitudType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Solicitud;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SolicitudType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titulo')
+            ->add('descripcion')
+            ->add('estado')
+            ->add('estudiante')
+            ->add('asignatura')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Solicitud::class,
+        ]);
+    }
+}
